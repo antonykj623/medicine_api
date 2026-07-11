02dfefd83931201b34a96f1a150e6a8be06c00e5
j2 infotech <j2info1994@gmail.com>
2026-07-11 15:11:14 +0530

    commited by antony

--- Db.py
+++ Db.py
@@ -3,16 +3,17 @@ import mysql.connector
 class Db:
     
     def __init__(self):
-        mydb=mysql.connector.connect(  host="localhost",
-  user="root",
-  password="root@1234")
+        mydb=mysql.connector.connect(  host="sql12.freesqldatabase.com",
+  user="sql12832775",
+  password="nDyArKF9pK",
+  )
 
         mycursor = mydb.cursor(dictionary=True)
         self.mycursor=mycursor
         self.mydb=mydb
 
-        self.mycursor.execute("CREATE DATABASE IF NOT EXISTS med_db")
-        self.mycursor.execute("USE med_db")
+        self.mycursor.execute("CREATE DATABASE IF NOT EXISTS sql12832775")
+        self.mycursor.execute("USE sql12832775")
         create_table_query = """
        CREATE TABLE IF NOT EXISTS medicine (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -30,7 +31,7 @@ class Db:
 
 
     def exceuteQuery(self,query,values):
-       self.mycursor.execute("USE med_db")
+       self.mycursor.execute("USE sql12832775")
        if values:
            
         self.mycursor.execute(query, values)
@@ -40,12 +41,12 @@ class Db:
 
 
     def fetchAll(self,query,values):
-        self.mycursor.execute("USE med_db")        
+        self.mycursor.execute("USE sql12832775")        
         self.mycursor.execute(query,values)
         return self.mycursor.fetchall()
 
     def fetchOrderByStock(self,query):
-         self.mycursor.execute("USE med_db") 
+         self.mycursor.execute("USE sql12832775") 
          self.mycursor.execute(query)
          return self.mycursor.fetchall()
 
@@ -54,7 +55,7 @@ class Db:
 
 
     def fetchone(self,query,values):
-         self.mycursor.execute("USE med_db")  
+         self.mycursor.execute("USE sql12832775")  
          self.mycursor.execute(query,values)      
          return self.mycursor.fetchone()    
 
